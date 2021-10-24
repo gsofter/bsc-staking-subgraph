5cc719370c4c96f9ef4b96b7b9b049bbf6a18ffa
gsofter <real.kingstar@outlook.com>
2021-10-23 19:45:07 -0500

    add subgraph

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,36 +1,36 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
   Staking,
   ClaimedGFX,
   ClaimedNFT,
   NewNFTStaked,
-  OwnershipTransferred
-} from "../generated/Staking/Staking"
-import { ExampleEntity } from "../generated/schema"
+  OwnershipTransferred,
+} from "../generated/Staking/Staking";
+import { NFTEntity, NewNFTStakedEntity } from "../generated/schema";
 
 export function handleClaimedGFX(event: ClaimedGFX): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = NFTEntity.load(event.transaction.from.toHex());
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    entity = new NFTEntity(event.transaction.from.toHex());
 
     // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity.count = BigInt.fromI32(0);
   }
 
   // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  // entity.count = entity.count + BigInt.fromI32(1);
 
   // Entity fields can be set based on event parameters
-  entity.nftOwner = event.params.nftOwner
-  entity.amount = event.params.amount
+  entity.nftOwner = event.params.nftOwner;
+  entity.amount = event.params.amount;
 
   // Entities can be written to the store with `.save()`
-  entity.save()
+  entity.save();
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
@@ -57,6 +57,11 @@ export function handleClaimedGFX(event: ClaimedGFX): void {
 
 export function handleClaimedNFT(event: ClaimedNFT): void {}
 
-export function handleNewNFTStaked(event: NewNFTStaked): void {}
+export function handleNewNFTStaked(event: NewNFTStaked): void {
+  let entity = new NewNFTStakedEntity(event.transaction.hash.toHex());
+  entity.nftOwner = event.params.nftOwner;
+  entity.nftId = event.params.nftId;
+  entity.nftImage = event.params.nftImage;
+}
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
